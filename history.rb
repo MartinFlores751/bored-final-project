f3b42b249423738846d17a16068e8f0421504071
Martin Jaime Flores Jr <mf34314@gmail.com>
2018-12-14 21:48:48 -0600

    Tiny change

--- history.rb
+++ history.rb
@@ -6,6 +6,6 @@ class History
     property :sheet_id, Integer #when you create a history object, query for the Serial id of the sheet and put it here
     property :created_at, DateTime  #time of transaction
     property :charge, String #money made from transaction
-    property :buyer, String
-    property :seller, String
+    property :buyer, Integer
+    property :seller, Integer
 end
